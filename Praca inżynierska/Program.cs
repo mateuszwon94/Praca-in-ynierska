a57b928a641c24498234ac3aabd7a81f4e6015ff
Mateusz <mateusz.winiarski.94@gmail.com>
2015-11-27 14:47:32 +0100

    Początek

---
+++ Praca inżynierska/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Praca_inżynierska {
+	class Program {
+		static void Main(string[] args) {
+			RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Praca inzynierska");
+            RectangleShape rec = new RectangleShape(new Vector2f(10, 10));
+			window.Closed += (o, e) => window.Close();
+
+			while (window.IsOpen) {
+				window.DispatchEvents();
+				window.Clear();
+				window.Draw(rec);
+				window.Display();
+			}
+		}
+	}
+}
